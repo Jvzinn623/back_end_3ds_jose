188ac0219a46fcf927675503fc5f18953903cc16
Jvzinn623 <jose.falavinha.santos@escola.pr.gov.br>
2026-02-23 12:36:32 +0000

    Criar variaveis para pessoa e produto

---
+++ variaveis/variavel-pessoa.js
@@ -0,0 +1,20 @@
+// , var, let, const, (Valores reservados da linguagem)
+// 0-9, 0-7, 0-1,0-9, A,B,C,D,E,F
+let nome = 'Isadora'         //string
+let sobrenome = 'Leite'      //string
+const cpf = "696969696-69"   //string
+const rg = '999999 ssp'      //string
+let idade = 69               //number
+let solteira = true          //boolean
+let nota = 6 + 7             //number
+
+console.log (nome)
+// imprima aqui todas as variaveis acima
+console.log(typeof nome)
+console.log(typeof sobrenome)
+console.log(typeof sobrenome)
+console.log(typeof cpf)
+console.log(typeof rg)
+console.log(typeof idade)
+console.log(typeof solteira)
+console.log(typeof nota)
---
+++ variaveis/variavel-produto.js
@@ -0,0 +1,17 @@
+let nomeProduto = "Livros"
+let preco = '14,99'
+let quantidade = '40'
+let local = "China"
+const loja = Culturas
+
+console.log(nomeProduto)
+console.log(preco)
+console.log(quantidade)
+console.log(local)
+console.log(loja)
+
+console.log("nome: " + nomeProduto)
+console.log("nome: " + preco)
+console.log("quantidade: " + quantidade)
+console.log("local:" + local)
+console.log("Culturas :" + Culturas)
