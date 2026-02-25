c105a99cc2cfb29bf4a41f4cc5f5afae84bfe416
Jvzinn623 <jose.falavinha.santos@escola.pr.gov.br>
2026-02-25 11:37:00 +0000

    Criar expressões de decisão condicional if-else

---
+++ condicionais/if-login.js
@@ -0,0 +1,8 @@
+let login = "gt123"
+let senha = "123456"
+
+if (login == "gt123") {
+    console.log = ("Login valido")
+} else {
+    console.log = ("Login invalido")
+}
---
+++ condicionais/if_else.js
@@ -0,0 +1,13 @@
+// if, else, else if, ternário
+// Serve para tmadacde decisão (se/senão)
+
+let idadePessoa = 101
+
+if (idadePessoa > 100){
+    console.log("Favor,aprensentar acompanhante")
+} else if (idadePessoa >= 17 && idadePessoa <= 100){
+    console.log("Bem vindo(a), a festa é sua!")
+    console.log("Festa sugerida > Boate!!!")
+} else {
+    console.log("Não é festa para menor de idade!")
+}
