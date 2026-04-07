0809dd5430971f9563710ef206d3a2299ec01b90
Jvzinn623 <jose.falavinha.santos@escola.pr.gov.br>
2026-04-07 12:18:26 +0000

    template

---
+++ funcoes/func-04-retorno.js
@@ -0,0 +1,6 @@
+function sobraDoSalario(meuSalario){
+    const contaDeLuz = 150.0
+    const aluguel = 1400.0
+    const resto = meuSalario - (contaDeLuz+aluguel)
+    console.log(`O que restou no fim do mês ${resto}`)
+}
---
+++ variaveis/template-desafio01.js
@@ -0,0 +1,9 @@
+const profissao = {
+    nome: "Profissão: Médico,",
+    salario: "Salário: R$10.000,00,",
+    formacao: "Formação: Ginecologista,",
+    AnosDeExperiencia: "Experiência: 12 anos.",
+}
+
+const perfil = `\n${profissao.nome} \n${profissao.salario} \n${profissao.formacao} \n${profissao.AnosDeExperiencia}`
+console.log(perfil)
