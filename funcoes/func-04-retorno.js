7422d9beefa1147948af87b915af981f6ab937e6
Jvzinn623 <jose.falavinha.santos@escola.pr.gov.br>
2026-04-08 12:37:14 +0000

    func-04

--- funcoes/func-04-retorno.js
+++ funcoes/func-04-retorno.js
@@ -2,5 +2,7 @@ function sobraDoSalario(meuSalario){
     const contaDeLuz = 150.0
     const aluguel = 1400.0
     const resto = meuSalario - (contaDeLuz+aluguel)
-    console.log(`O que restou no fim do mês ${resto}`)
+    return `O que restou no fim do mês ${resto}`
 }
+const minhasSobras = sobraDoSalario(1700.0)
+console.log(sobraDoSalario(5000.0))
