656723a376895f5d18257c13ba7a2e2c6f3c9057
Jvzinn623 <jose.falavinha.santos@escola.pr.gov.br>
2026-05-13 11:40:00 +0000

    módulo gatos

---
+++ 2 TRIMESTRE/modulos/mod02-ESM/main-mod-gatos.js
@@ -0,0 +1,7 @@
+import {banhoGato, tosaGato, alimentarGato} from './mod-gatos.js'
+
+let racaViraLata = 'Vira-lata'
+
+console.log(banhoGato('Siamês', 'Completo'))
+console.log(tosaGato('Siamês','Completa'))
+console.log(alimentarGato('Siâmes','Prime'))
--- 2 TRIMESTRE/modulos/mod02-ESM/mod-gatos.js
+++ 2 TRIMESTRE/modulos/mod02-ESM/mod-gatos.js
@@ -1,10 +1,10 @@
-let racaViraLata = 'Vira-lata'
-export function banhoGato(racaGato, tipoBanho){
 
+export function banhoGato(racaGato, tipoBanho){
+  return `Gato ${racaGato} fez um banho tipo: ${tipoBanho}`
 }
 export function tosaGato(racaGato, tipoTosa){
-
+  return `Gato ${racaGato} fez uma tosa tipo: ${tipoTosa}`
 }
 export function alimentarGato(racaGato, tipoRacao){
-
+  return `Gato ${racaGato} come ração: ${tipoRacao}`
 }
