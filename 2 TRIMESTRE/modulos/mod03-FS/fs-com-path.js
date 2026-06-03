5e102d8f724c6142b20c4833d6e4f4383ddc41fa
Jvzinn623 <jose.falavinha.santos@escola.pr.gov.br>
2026-06-03 12:32:23 +0000

    03/06/2026

--- 2 TRIMESTRE/modulos/mod03-FS/fs-com-path.js
+++ 2 TRIMESTRE/modulos/mod03-FS/fs-com-path.js
@@ -6,7 +6,14 @@ const path = require('path')
 
 // Salvar o caminho em uma variável
 //const caminho = path.join(__dirname, 'README.md')//Markdown, text,
+try {
 const caminho = path.resolve(__dirname, 'README.md')// Abrir, ler, deletar, editar
 const arquivo = fs.readFileSync(caminho, 'utf-8')
-
 console.log(arquivo)
+}catch(erro){
+    console.log('-------------------------')
+    console.warn("Atenção!"/erro.path)
+    throw new Error("Algo deu errado na sua operação!");
+
+    console.log('-------------------------')
+}
